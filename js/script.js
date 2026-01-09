078b8ff2214aa939cd76f18fe793c92be11abf9b
Aiden Chen <197520251+acstartup@users.noreply.github.com>
2026-01-09 09:42:27 -0500

    feat(sucessful-login): 6....

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 
 
-// function: set user info to json
+// function: set user info to json (sign up)
 
-// function: import into system
+// function: import into system (sign up)
+
+// function: check if user is in system (log in)
+
+// function: returns feedback to user / alerts about password or email no account or successful log in (log in)
